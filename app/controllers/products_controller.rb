b27e48b3cc115345f4993643c06c535659c3068f
Nathan Brownstein <nana8901@gmail.com>
2022-07-28 14:31:07 -0400

    added a patch to update/edit photos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,7 +20,17 @@ class ProductsController < ApplicationController
     )
     product.save
     render json: {new_product: product}
-    
+  end
+
+  def update
+    p params 
+    product = Product.find_by(id: params[:id])
+    product.name = params[:name]
+    product.price = params[:price]
+    product.image_url = params[:image_url]
+    product.description = params[:description]
+    product.save
+    render json: product.as_json
   end
 
 end 
