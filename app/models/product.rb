63c9b90be7eafbfde2742221005608b75419d36f
Nathan Brownstein <nana8901@gmail.com>
2022-08-01 14:59:14 -0400

    added a discounted method to the product model and to the show method in product_controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,14 @@ class Product < ApplicationRecord
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
   end
+
+  def is_discounted?
+    if price < 10
+      return true
+    else
+      return false
+    end
+  end
   
+
 end
