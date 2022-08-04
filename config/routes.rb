8d1320c5d2b73522a3fd5d785c87661be82b66bc
Nathan Brownstein <nana8901@gmail.com>
2022-08-04 14:44:51 -0400

    added CRUD for suppliers to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
   post "products" => "products#create"
   patch "products/:id" => "products#update"
   delete "products/:id" => "products#destroy"
+  get "/suppliers" => "suppliers#index"
+  get "/suppliers/:id" => "suppliers#show"
+  post "suppliers" => "suppliers#create"
+  patch "suppliers/:id" => "suppliers#update"
+  delete "suppliers/:id" => "suppliers#destroy"
+
 end
