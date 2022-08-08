b33613b0b72bb3dcb94526dba59fa669b9b3530a
Nathan Brownstein <nana8901@gmail.com>
2022-08-08 15:13:47 -0400

    adding in tax to order creation, adding in auto checking user with JWT

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,15 @@
 class OrdersController < ApplicationController
   def create
+    product = Product.find_by(params[:product_id])
+    subtotal = product.pr
     order = Order.new({
-      user_id: params[:user_id]
-      product_id: params[:product_id]
+      user_id: current_user.id,
+      product_id: params[:product_id],
       quantity: params[:quantity]
     }) 
+
     if order.save
-      render json: {success: "Order placed!"}
+      render json: {success: "ordered #{:quantity}"}
     else
       render json: {error: "Something went wrong, try again?"}
     end
