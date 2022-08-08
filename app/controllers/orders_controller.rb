0cf6e9585d7f93ae18fc9e6a4874923d9d67e2a0
Nathan Brownstein <nana8901@gmail.com>
2022-08-08 15:24:19 -0400

    added subtotal, etc to order, fixed bugs

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,23 @@
 class OrdersController < ApplicationController
   def create
-    product = Product.find_by(params[:product_id])
-    subtotal = product.pr
+    product = Product.find_by(id: params[:product_id])
     order = Order.new({
       user_id: current_user.id,
       product_id: params[:product_id],
-      quantity: params[:quantity]
+      quantity: params[:quantity],
+      subtotal: product.price * params[:quantity],
+      tax: product.tax * params[:quantity],
+      total: product.total * params[:quantity]
     }) 
 
     if order.save
-      render json: {success: "ordered #{:quantity}"}
+      render json: {success: "ordered #{params[:quantity]}"}
     else
       render json: {error: "Something went wrong, try again?"}
     end
+  end
+
+  def index
+    code
   end
 end
