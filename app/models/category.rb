c42ee7fd9f2441052eae47312d173655e0995d8c
Nathan Brownstein <nana8901@gmail.com>
2022-08-10 14:06:05 -0400

    adding 'belongs_to' and 'has_many' relationships to CategoryProduct and Category and Product

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :category_product
+
 end
--- app/models/category_product.rb
+++ app/models/category_product.rb
@@ -1,2 +1,5 @@
 class CategoryProduct < ApplicationRecord
+  belongs_to :product
+  belongs_to :category
+
 end
